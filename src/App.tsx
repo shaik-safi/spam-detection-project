5aa922c2502cdf9b3881a3bc65415a4c560a480f
Minisha-N <minisha9353@gmail.com>
2025-04-10 16:27:22 +0530

    login & signup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { ThemeProvider } from './components/theme-provider'
-import './App.css'
-import Dashboard from './pages/Dashboard'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { ThemeProvider } from "./components/theme-provider"
+import Login from "./pages/LoginPage"
+import Dashboard from "./pages/Dashboard"
+import Signup from "./pages/SignupPage"
 
 function App() {
-
   return (
-    <>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <Dashboard/>
-      </ThemeProvider>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   )
 }
 
---
+++ src/pages/SignupPage.tsx
@@ -0,0 +1,12 @@
+// src/pages/login.tsx
+import {SignupForm} from "@/components/SignupForm"
+
+export default function Signin() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignupForm />
+      </div>
+    </div>
+  )
+}
